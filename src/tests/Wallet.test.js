acfe297233e80e77ed772450e12fbc57f3314465
Paloma Wink <paloma.wink@hotmail.com>
2023-05-18 18:07:50 -0300

    Inicio do requisito 10

--- src/tests/Wallet.test.js
+++ src/tests/Wallet.test.js
@@ -1,14 +1,26 @@
 import React from 'react';
-import { screen } from '@testing-library/react';
+import { screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { act } from 'react-dom/test-utils';
 import { renderWithRouterAndRedux } from './helpers/renderWith';
+import mockData from './helpers/mockData';
 import App from '../App';
+import Wallet from '../pages/Wallet';
 
-// Verificar os campos
+// Verificar os campos ok
 // Verificar a atualizacao do valor no header
-// Verifica o preenchimento dos inputs
-// Verifica a atualizacao do estado global
+// Verifica se a tabela e alimentada corretamente
+// Verifica se ao clicar em Editar os valores voltam para os campos e a tabela e atualizada
+// Verifica se ao clicar em Excluir aquela linha da tabela some e o valor do header e atualizado
+
+beforeEach(() => {
+  jest.spyOn(global, 'fetch');
+  global.fetch.mockResolvedValue({
+    json: jest.fn().mockResolvedValue(mockData),
+  });
+});
+
+afterEach(jest.restoreAllMocks);
 
 test('Verifica se todos os componentes sao renderizados na tela', () => {
   renderWithRouterAndRedux(<App />);
@@ -22,9 +34,55 @@ test('Verifica se todos os componentes sao renderizados na tela', () => {
   act(() => userEvent.click(button));
 
   screen.getByText(/email:teste@teste.com/i);
-  screen.getByText(/despesa total: r\$0\.00/i);
   screen.getByText(/brl/i);
-  screen.getByRole('textbox', { name: /valor:/i });
-  screen.getByRole('textbox', { name: /descrição:/i });
-  screen.getByRole('button', { name: /adicionar despesa/i });
+  screen.getByTestId('currency-input');
+  screen.getByTestId('method-input');
+  screen.getByTestId('tag-input');
 });
+
+test('Verifica se o botao de "Adicionar despesas" está funcionando e o header é atualizado corretamente ao adicionarmos uma despesa', async () => {
+  const expenseOne = {
+    id: 0,
+    valor: 70.00,
+    descricao: 'Calça jeans',
+    moeda: 'USD',
+    metodoPagamento: 'Dinheiro',
+    tag: 'Alimentação',
+    nomeMoeda: mockData.USD.name,
+    cambioUtilizado: mockData.USD.ask,
+    valorConvertido: 332.71,
+    moedaConversao: 'Real',
+  };
+
+  renderWithRouterAndRedux(<Wallet />);
+  // Aqui eu sigo adicionando as despesas e testando o botao para verificar se esta tudo funcionando
+  const value = screen.getByRole('spinbutton', { name: /valor:/i });
+  const description = screen.getByRole('textbox', { name: /descrição:/i });
+  const button = screen.getByRole('button', { name: /adicionar despesa/i });
+
+  userEvent.type(value, expenseOne.valor);
+  userEvent.type(description, expenseOne.descricao);
+
+  act(() => userEvent.click(button));
+
+  screen.getByText(expenseOne.valor);
+  screen.getByText(expenseOne.descricao);
+  screen.getByText(expenseOne.moeda);
+  screen.getByText(expenseOne.metodoPagamento);
+  screen.getByText(expenseOne.tag);
+  await waitFor(() => {
+    expect(expenseOne.nomeMoeda).toBeInTheDocument();
+  });
+});
+
+/* test('Verifica se a tabela e alimentada corretamente ao adicionarmos uma despesa', () => {
+
+});
+
+test('Verifica se ao clicar no botao de editar despesa as informacoes voltam para os inputs e o seu valor e atualizado na tabela', () => {
+
+});
+
+test('Verifica se ao clicar no botao de excluir, o item some da lista e o header seja atualizado', () => {
+
+}); */
